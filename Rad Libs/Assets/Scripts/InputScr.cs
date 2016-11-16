d7d299ee1881e37b3524cbbd71b73621ede178b6
MaxGWagner <mw631214@ohio.edu>
2016-11-16 00:55:50 -0500

    First Iteration
    Input and Output implemented
    Timer created but not used
    Input array needed

---
+++ Rad Libs/Assets/Scripts/InputScr.cs
@@ -0,0 +1,32 @@
+﻿//right now this script takes input and puts out the last word as output
+//but it still needs to load input into an array or string
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;using System;
+using System.Collections.Generic;
+
+ 
+public class InputScr : MonoBehaviour {
+ 
+    InputField input;
+    InputField.SubmitEvent se;
+    public Text output;
+	string fullText = "";
+     
+    void Start () {
+        input = gameObject.GetComponent<InputField>();//input is what goes in the input box
+        se = new InputField.SubmitEvent();
+        se.AddListener(SubmitInput);
+        input.onEndEdit = se;
+    }
+ 
+    private void SubmitInput(string arg0)
+    {
+        string newText = arg0;
+        output.text = newText;
+        input.text = "";
+        input.ActivateInputField();
+    }
+     
+ 
+}
---
+++ Rad Libs/Assets/Scripts/Timer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class Timer : MonoBehaviour {
+	
+	public float timeLimit = 15.0f;
+	public Text timerLabel;
+
+	
+	void Start () 
+	{
+	
+	}
+	
+	void Update () 
+	{
+		
+		timeLimit -= Time.deltaTime;
+		int roundedTL = (int)(timeLimit + 0.5f);
+		if (timeLimit < 0)
+			timeLimit = 0;
+		
+		if (timeLimit < 0)
+		{
+			//Debug.Log("Time's Up!");
+		}
+		
+		timerLabel.text = Convert.ToString(roundedTL);
+	}
+}
